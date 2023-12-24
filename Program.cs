35551de7e155b7f9c02657411350db32cbdb7032
ALLUK <lam83@bk.ru>
2023-12-25 00:09:54 +0500

    1 решение задачи кв уравнения

--- Program.cs
+++ Program.cs
@@ -8,51 +8,58 @@ namespace ConsoleAppLearnClass
 {
     class Address
     {
-        public string Name="";
-        public string Surname="";
-        public string Country="";
-        public string Region="";
-        public string City = "";
-        public string Street = "";
-        public int NumberBuilding = 0;
-        public int NumberFlat = 0;
+        /// <summary>
+        /// Задание 9. Создание класса для арифметических вычислений
+        /// Рассмотрим Программу решения квадратного уравнения с использование класса KvUr
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Address adr = new Address("Aleksey","Lukin","Russia","Moscow","Moscow","Lesnaya Street",6,11);
-            Console.WriteLine(adr.Name);
-            Console.WriteLine(adr.Surname);
-            Console.WriteLine(adr.Country);
-            Console.WriteLine(adr.Region);
-            Console.WriteLine(adr.City);
-            Console.WriteLine(adr.Street);
-            Console.WriteLine(adr.NumberBuilding);
-            Console.WriteLine(adr.NumberFlat);
+            Console.WriteLine("Введите значения a,b и c больше нуля: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+
+             new KvUr(a, b, c);
+            
+
             Console.ReadLine();
-
         }
-        
-        // Обратите внимание на ряд особенностей конструктора. 
-        //Во-первых, конструктор называется как класс.Раз наш класс называется Adress, значит и конструктор должен
-        //называться точно также.
-        //И во-вторых, конструктор, в отличие от других методов класса, не возвращает никакого значения (даже типа void). 
+    }
+    class KvUr
+    {
+        private double a;
+        private double b;
+        private double c;
 
-        public Address(string Name,string Surname, string Country, string Region, string City, string Street, int numberBuilding, int NumberFlat)
+        public KvUr(double a, double b, double c)
         {
-            this.Name = Name;
-            this.Surname = Surname;
-            this.Country = Country; 
-            this.Region = Region;
-            this.City = City;   
-            this.Street = Street;
-            this.NumberBuilding=6;
-            this.NumberFlat = 11;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+            double Discriminant = Math.Pow(b, 2) - (4 * a * c);
+
+            if (Discriminant > 0)
+            {
+                double xOne = (-b + Math.Sqrt(Discriminant)) / 2 / a;
+                double xTwo = (-b - Math.Sqrt(Discriminant)) / 2 / a;
+                Console.WriteLine($"Первое неизвестное число равно {0}, второе {1}", xOne, xTwo);
+            }
+            if (Discriminant == 0)
+            {
+                double x = (-b) / 2 / a;
+
+                Console.WriteLine($"Неизвестное число равно {0}", x);
+            }
+            if (Discriminant < 0)
+            {
+                Console.WriteLine($"Дискриминант {Discriminant} меньше нуля, следовательно уравнение не имеет действительных корней");
+            }
         }
     }
 }
-//Что делает наш конструктор? Он записывает передаваемые в него параметры во внутренние переменные 
-//класса.Обратите внимание, что называются они одинаково - NumberFlat и NumberFlat, Name и Name и т.д. Компилятор сначала 
-//ищет локальную переменную с таким именем, и, если не находит, то переменную класса. Поэтому NumberFlat (и другие) -
-//это передаваемые в конструктор параметры.
-//Если же нам надо сослаться на переменную класса (при существовании переменной с таким же именем,
-//как и передаваемый в функцию параметр), то мы используем ключевое слово this. 
-//Оно всегда указывает на текущий экземпляр нашего класса. Таким образом в строчках
+        
+
+
+
